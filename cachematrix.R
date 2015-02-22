e4a5e08006414c6d8f833182f787508563ad1e6a
KimBannon <Kim.bannon@gmail.com>
2015-02-23 09:31:13 +1300

    final typo correction in comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,8 +5,8 @@
 makeCacheMatrix <- function(x = matrix()) {
 	inverse <- NULL  ##sets inverse to NULL as placeholder for future inverse
 
-## the set function allows you to define the matrix. the global << is used as 
-## it is defined outside the function
+## the set function allows you to define the matrix. The global << is used so 
+## you can use the object outside of the function
 	set <- function(y) {
 		x <<- y
 		inverse <<-NULL  ##looks in parent environment
@@ -33,12 +33,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## This function computes the inverse of the special "matrix" returned 
 ## by makeCacheMatrix above. If the inverse has already been calculated
-## and the matrix has not changed), then the cacheSolve retrieves the 
+## and the matrix has not changed, then the cacheSolve retrieves the 
 ## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-## Returns a matrix that is the inverse of 'x'
 
+	## Returns a matrix that is the inverse of 'x'
 	inverse <- x$getinverse()
 	
 	## if the inverse matrix is not empty then obtain the cached inverse
